7066696b3637fefc8d81d249178e84a104927d58
Divya Shanmugam <divyashan@gmail.com>
2017-10-25 12:55:54 -0400

    Added in Rand Index eval_clustering method

--- models/utils.py
+++ models/utils.py
@@ -2,8 +2,11 @@ import numpy as np
 import tensorflow as tf
 from itertools import product
 import scipy
+import pdb
+from scipy.misc import comb
+import matplotlib.pyplot as plt
 
-NUM_DIFF=30
+NUM_DIFF=10
 UCR_DATASETS = ['50Words', 'Adiac', 'ArrowHead', 'Beef', 'BeetleFly', 'BirdChicken', 'CBF', 'Car', 'ChlorineConcentration', 
                           'CinC_ECG_torso', 'Coffee', 'Computers', 'Cricket_X', 'Cricket_Y', 'Cricket_Z', 'DiatomSizeReduction', 
                           'DistalPhalanxOutlineAgeGroup', 'DistalPhalanxOutlineCorrect', 'DistalPhalanxTW', 'ECG200', 'ECG5000', 'ECGFiveDays', 
@@ -17,6 +20,10 @@ UCR_DATASETS = ['50Words', 'Adiac', 'ArrowHead', 'Beef', 'BeetleFly', 'BirdChick
                           'Swedish Leaf', 'Symbols', 'Synthetic Control', 'ToeSegmentation1', 'ToeSegmentation2', 'Trace', 'Two Patterns', 
                           'TwoLeadECG', 'UWaveGestureLibraryAll', 'Wafer', 'Wine', 'WordSynonyms', 'Worms', 'WormsTwoClass', 'Yoga', 
                           'uWaveGestureLibrary_X', 'uWaveGestureLibrary_Y', 'uWaveGestureLibrary_Z']
+MV_DATASETS = ['arabic_digits', 'auslan', 'trajectories', 'libras', 'wafer', 'ecg']
+NEG_LABEL = 1
+SAME_LABEL = 0
+
 def create_triplets(x, digit_indices, labels):
     '''Positive and negative pair creation.
     Alternates between positive and negative pairs.
@@ -30,7 +37,9 @@ def create_triplets(x, digit_indices, labels):
         eq_pairs = filter(lambda x: x[0] < x[1], eq_pairs)
         for pair in eq_pairs:
             doublet = [x[pair[0]], x[pair[1]]]
-            for i in range(j+1,len(labels)):
+            for i, d_c in enumerate(labels):
+                if d == d_c:
+                    continue 
                 idx = i % len(labels)
                 new_triplets = []
                 n_samples = min(NUM_DIFF,len(digit_indices[idx]))
@@ -54,7 +63,9 @@ def create_triplet_idxs(x, digit_indices, labels):
         eq_pairs = filter(lambda x: x[0] < x[1], eq_pairs)
         for pair in eq_pairs:
             doublet = [pair[0], pair[1]]
-            for i in range(j+1,len(labels)):
+            for i in range(len(labels)):
+                if i == j:
+                    continue
                 idx = i % len(labels)
                 new_triplets = []
                 n_samples = min(NUM_DIFF,len(digit_indices[idx]))
@@ -64,6 +75,50 @@ def create_triplet_idxs(x, digit_indices, labels):
                 triplets.extend(new_triplets)
 
     return np.array(triplets)
+
+def create_pairs(x, digit_indices, labels):
+    '''Positive and negative pair creation.
+    Alternates between positive and negative pairs.
+    '''
+    # todo: turn this into indices
+    n_classes = len(labels)
+    n = min([len(digit_indices[d]) for d in range(n_classes)]) - 1
+    pairs = []
+    tags = []
+    for j, d in enumerate(labels):
+        eq_pairs = list(product(digit_indices[j], digit_indices[j]))
+        eq_pairs = filter(lambda x: x[0] < x[1], eq_pairs)
+        for pair in eq_pairs:
+
+            pairs.append([x[pair[0]], x[pair[1]]])
+            tags.append(SAME_LABEL)
+        for i in range(j+1, len(labels)):
+            diff_pairs = list(product(digit_indices[j], digit_indices[i]))
+            diff_pairs = diff_pairs
+            for pair in diff_pairs:
+                pairs.append([x[pair[0]], x[pair[1]]])
+                tags.append(NEG_LABEL)
+    return np.array(pairs), np.array(tags)
+
+def create_pair_idxs(x, digit_indices, labels):
+    '''Positive and negative pair creation.
+    Alternates between positive and negative pairs.
+    '''
+    # todo: turn this into indices
+    n_classes = len(labels)
+    n = min([len(digit_indices[d]) for d in range(n_classes)]) - 1
+    pairs = []
+    tags = []
+    for j, d in enumerate(labels):
+        eq_pairs = list(product(digit_indices[j], digit_indices[j]))
+        eq_pairs = filter(lambda x: x[0] < x[1], eq_pairs)
+        pairs.extend(eq_pairs)
+        tags.extend([SAME_LABEL for x in eq_pairs])
+        for i in range(j+1, len(labels)):
+            diff_pairs = list(product(digit_indices[j], digit_indices[i]))
+            pairs.extend(diff_pairs)
+            tags.extend([NEG_LABEL for x in diff_pairs])
+    return np.array(pairs), np.array(tags)
 
 
 
@@ -82,7 +137,7 @@ def next_batch_from_idx(s,e,inputs,X):
     input2 = X[inputs[s:end_ind,1]]
     input3 = X[inputs[s:end_ind,2]]
     return input1,input2,input3
-
+"""
 def plot_filters(plot_row, n_filters, W):
   f, axarr = plt.subplots(1, n_filters)
   for f_num in range(n_filters):
@@ -92,6 +147,23 @@ def plot_filters(plot_row, n_filters, W):
       plt.setp([axarr[f_num].get_xticklabels()], visible=False)
       if not f_num == 0:
           plt.setp([axarr[f_num].get_yticklabels()], visible=False)
+  f.subplots_adjust(hspace=0)  #No horizontal space between subplots
+  f.subplots_adjust(wspace=0)
+  plt.show()
+"""
+
+def plot_filters(n_filters, W):
+  f, axarr = plt.subplots(1, n_filters)
+  for f_num in range(n_filters):
+    plot_f = np.squeeze(W[:,:,f_num])
+    if n_filters > 1:
+      axarr[f_num].plot(plot_f)
+      plt.setp([axarr[f_num].get_xticklabels()], visible=False)
+    else:
+      axarr.plot(plot_f)
+      plt.setp([axarr.get_xticklabels()], visible=False)
+    if not f_num == 0:
+        plt.setp([axarr[f_num].get_yticklabels()], visible=False)
   f.subplots_adjust(hspace=0)  #No horizontal space between subplots
   f.subplots_adjust(wspace=0)
   plt.show()
@@ -125,6 +197,19 @@ def classify_sample(output, train_embedding, tr_y):
     dists = [np.linalg.norm(output-row) for row in train_embedding]
     return tr_y[np.argmin(dists)]
 
+
+def standardize_ts_lengths(X, n):
+    new_X = np.zeros((len(X), X[0].shape[1], n))
+    for i, v in enumerate(X):
+        new_X[i] = np.append(v.T, np.zeros((v.shape[1], n-v.shape[0])), axis = 1)
+    return new_X
+
+def standardize_ts_lengths_1(X, n):
+    new_X = np.zeros((len(X), n, X[0].shape[1]))
+    for i, v in enumerate(X):
+        new_X[i] = np.append(v, np.zeros((n-v.shape[0], v.shape[1])), axis = 0)
+    return new_X
+
 def produce_pair_labels(labels):
     match_list = []
     for i in range(len(labels)):
@@ -134,3 +219,49 @@ def produce_pair_labels(labels):
             else:
                 match_list.append(1)
     return np.array(match_list)
+
+def rebase_labels(y, labels):
+    new_y = [np.argwhere(labels == y_val)[0][0] for y_val in y]
+    return np.array(new_y)
+
+
+
+# There is a comb function for Python which does 'n choose k'                                                                                            
+# only you can't apply it to an array right away                                                                                                         
+# So here we vectorize it...                                                                                                                             
+def myComb(a,b):
+  return comb(a,b,exact=True)
+
+vComb = np.vectorize(myComb)
+
+def get_tp_fp_tn_fn(cooccurrence_matrix):
+  tp_plus_fp = vComb(cooccurrence_matrix.sum(0, dtype=int),2).sum()
+  tp_plus_fn = vComb(cooccurrence_matrix.sum(1, dtype=int),2).sum()
+  tp = vComb(cooccurrence_matrix.astype(int), 2).sum()
+  fp = tp_plus_fp - tp
+  fn = tp_plus_fn - tp
+  tn = comb(cooccurrence_matrix.sum(), 2) - tp - fp - fn
+
+  return [tp, fp, tn, fn]
+
+def eval_clustering(pred_labels, labels):
+  labels = np.array([int(x) for x in labels])
+  pred_labels = np.array(pred_labels)
+  kmean_sort_ind = np.argsort(labels)
+  pdb.set_trace()
+  pred_labels = pred_labels.labels_[kmean_sort_ind]
+
+  labels = labels[kmean_sort_ind]
+
+  unique_labels = np.unique(labels)
+  labels = rebase_labels(labels, unique_labels)
+  pred_labels -= np.min(pred_labels)
+
+  co_matrix = np.zeros((len(np.unique(labels)), len(np.unique(pred_labels)) ))
+  for i,x in enumerate(pred_labels):
+    co_matrix[int(labels[i])][x] += 1
+  tp, fp, tn, fn = get_tp_fp_tn_fn(co_matrix)
+
+  nmi = normalized_mutual_info_score(pred_labels, labels)
+  print 'Rand Index: ', rand_ind
+  return rand_ind
